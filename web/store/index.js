8c26bb8bcc5d3d93d5ff903c15a31c71ad8affe6
Kaushik2926 <baugkaushik147@gmail.com>
2020-12-25 11:49:38 +0530

    Pushed Changes
    Deleted extra folder named web

--- web/store/index.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import showCart from '../reducers/showCart'
-import {createStore} from 'redux';
-
-const store = createStore(showCart)
-
-export default store;
